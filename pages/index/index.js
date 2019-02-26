ff4011b0b3dc42b58524d1f3b9026ad9c643f8e3
ATQQ <2604395430@qq.com>
2019-02-26 23:17:30 +0800

    引入主页静态资源

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,13 +4,59 @@ const app = getApp()
 const { $Toast } = require('../../source/plunge/iview/base/index');
 Page({
   data: {
-    bottomcurrent:'',
+    bottomcurrent: '',
     inputShowed: false,
     inputVal: "",
-    power:'',
-    swiperItems:[
+    power: '',
+    itabsData:[
       {
-        src:'../../source/image/swiper/test1.png'
+        key: 'tab0',
+        title: '全部'
+      },
+      {
+        key:'tab1',
+        title:'评价'
+      },
+      {
+        key: 'tab2',
+        title: '地域'
+      },
+      {
+        key: 'tab3',
+        title: '价格'
+      },
+      {
+        key:"tab4",
+        title:"销量"
+      },
+      {
+        key: "tab5",
+        title: "好评"
+      },
+      {
+        key: "tab6",
+        title: "推荐"
+      }
+    ],
+    recommendItems: [
+      {
+        title: 'FBK调货',
+        src: [
+          '../../source/image/goods4.png',
+          '../../source/image/goods5.png'
+        ]
+      },
+      {
+        title: '分蛋糕',
+        src: [
+          '../../source/image/goods6.png',
+          '../../source/image/goods7.png'
+        ]
+      }
+    ],
+    swiperItems: [
+      {
+        src: '../../source/image/swiper/test1.png'
       },
       {
         src: '../../source/image/swiper/test2.png'
@@ -19,25 +65,48 @@ Page({
         src: '../../source/image/swiper/test3.png'
       }
     ],
-    notice_text:"测试内容",
-    noticeArray:['111','asqee','fdfdg中文'],
+    notice_text: "聚龙金融设备股份有限公司与俄罗斯银行达成合作协议，出口点钞机1万台。",
+    noticeArray: ['111', 'asqee', 'fdfdg中文'],
     tabscurrent: 'tab1',
-    tabscurrent_scroll: 'tab1'
+    tabscurrent_scroll: 'tab1',
+    goodsData:[
+      {
+        src:'../../source/image/goods1.png',
+        title:'康艺JBYD-HT 点钞机',
+        price:1380,
+        sell:123
+      },
+      {
+        src: '../../source/image/goods2.png',
+        title: '智利进口红酒',
+        price: 99,
+        sell: 532
+      }
+      ,{
+        src:'../../source/image/goods3.png',
+        title:'爱他美（Aptamil） 澳新爱他美金装版 幼儿配方奶粉 3段（12-36个月） 900g',
+        price: 150,
+        sell: 139
+      }
+    ]
+  },
+  randomNumber:function () {
+    return Math.floor(Math.random()*999); 
   },
   // 小功能列表
-  test:function(res){
+  test: function (res) {
     console.log(res.currentTarget.dataset.key);
   },
   tabsChangeScroll({ detail }) {
     this.setData({
-      tabscurrent_scroll:detail.key
+      tabscurrent_scroll: detail.key
     })
     console.log(detail);
   },
-  clickScan:function(res){
+  clickScan: function (res) {
     console.log("click scan");
     wx.scanCode({
-      success:(res)=>{
+      success: (res) => {
         console.log(res);
       }
     })
@@ -49,8 +118,8 @@ Page({
     });
   },
   sureInput: function () {
-    var val=this.data.inputVal;
-    console.log('确定'+val);
+    var val = this.data.inputVal;
+    console.log('确定' + val);
     this.setData({
       inputVal: "",
       inputShowed: false
@@ -68,11 +137,11 @@ Page({
   },
   //事件处理函数
   onLoad: function () {
-    var that=this;
-    var count=0;
+    var that = this;
+    var count = 0;
     this.setData({
       bottomcurrent: app.globalData.current,
-      power:app.globalData.power
+      power: app.globalData.power
     })
     $Toast({
       content: '加载中',
@@ -85,14 +154,14 @@ Page({
   onReady: function () {
     $Toast.hide();
   },
-  setNotice:function(index){
-    var that=this;
+  setNotice: function (index) {
+    var that = this;
 
-    setTimeout((index)=>{
+    setTimeout((index) => {
       that.setData({
 
       })
-    },2000)
+    }, 2000)
   },
   // 底部tabbars
   bottomPageChange({ detail }) {
